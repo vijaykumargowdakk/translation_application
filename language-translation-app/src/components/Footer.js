44684c7abf0553f59747168116bfbfbcf2f4340a
vijaykumargowdakk <rvit21bis065.rvitm@rvei.edu.in>
2024-11-02 23:52:23 +0530

    variations of UI

--- language-translation-app/src/components/Footer.js
+++ language-translation-app/src/components/Footer.js
@@ -3,13 +3,15 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>Created by Vijay Kumar</p>
-      <a href="https://github.com/vijaykumargowdakk" className="footer-link">
-        <i className="fab fa-github"></i>
-      </a>
-      <a href="mailto:rvit21bis065.rvitm@rvei.edu.in" className="footer-link">
-        <i className="fas fa-envelope"></i>
-      </a>
+      <p>Created by Vijay Kumar Gowda K K</p>
+      <div className="icon-container">
+        <a href="https://github.com/vijaykumargowdakk" className="footer-link" aria-label="GitHub">
+          <i className="fab fa-github"></i>
+        </a>
+        <a href="mailto:rvit21bis065.rvitm@rvei.edu.in" className="footer-link" aria-label="Email">
+          <i className="fas fa-envelope"></i>
+        </a>
+      </div>
     </footer>
   );
 }
